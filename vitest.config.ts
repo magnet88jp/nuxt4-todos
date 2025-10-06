b6af206f10be1c62c4ff480cf8ace892ae9afd34
Shinsaku Nishino <magnet@MagsBookPro.local>
2025-10-06 22:48:16 +0900

    update by nuxt4-test-utils

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,14 +4,14 @@ export default defineVitestConfig({
   test: {
     environment: 'nuxt',
     // you can optionally set Nuxt-specific environment options
-    environmentOptions: {
-      nuxt: {
-        // rootDir: fileURLToPath(new URL('./playground', import.meta.url)),
-        domEnvironment: 'happy-dom', // 'happy-dom' (default) or 'jsdom'
-        // overrides: {
-        //   // other Nuxt config you want to pass
-        // }
-      }
-    }
+    // environmentOptions: {
+    //   nuxt: {
+    //     rootDir: fileURLToPath(new URL('./playground', import.meta.url)),
+    //     domEnvironment: 'happy-dom', // 'happy-dom' (default) or 'jsdom'
+    //     overrides: {
+    //       // other Nuxt config you want to pass
+    //     }
+    //   }
+    // }
   },
 })
